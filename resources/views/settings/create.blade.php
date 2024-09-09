3ae5e6df3279ffe61619277dd31dce47665495c0
George Chitechi <georgechitechi@gmail.com>
2024-09-09 23:39:56 +0300

    Initial Commit

--- resources/views/settings/create.blade.php
+++ resources/views/settings/create.blade.php
@@ -7,7 +7,7 @@
         @csrf
 
         <div class="form-group g-2">
-            <label for="setting1">Setting 1:</label>
+            <label for="setting1">Setting One:</label>
             <div class="form-check form-check-inline ms-4">
                 <input class="form-check-input" type="radio" name="settings[setting1]" value="option1">
                 <label class="form-check-label" for="inlineRadio1">Option 1</label>
