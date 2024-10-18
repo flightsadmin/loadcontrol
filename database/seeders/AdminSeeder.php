8517a09e4054ae5246fc218d7dfe1b96ebe749a3
George Chitechi <georgechitechi@gmail.com>
2024-10-18 08:34:28 +0300

    Initial Commit

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -3,11 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\PermissionRegistrar;
 
 class AdminSeeder extends Seeder
